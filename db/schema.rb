3e9b219cbfc6ec37a23cb8b62b94041683446cf7
kawasaki yuto <smile.holic.07p15@gmail.com>
2016-02-28 18:49:52 +0900

    サインインきのうの追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160228084156) do
+ActiveRecord::Schema.define(version: 20160228091728) do
 
   create_table "twigs", force: :cascade do |t|
     t.string   "user_id",    limit: 255
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20160228084156) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "thumbnail",              limit: 255
+    t.string   "nickname",               limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
