70df09d898074e852ccdf439355642445022cd69
kawasaki yuto <smile.holic.07p15@gmail.com>
2016-02-29 12:46:44 +0900

    アソシエーション追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @nickname = current_user.nickname
-    @twigs = Twig.where(user_id: current_user.id).order("created_at DESC")
+    @twigs = current_user.twigs.order("created_at DESC")
   end
 end
