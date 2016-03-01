ba017f64e9f542b28c8a44632f43489d7a65767b
kawasaki yuto <smile.holic.07p15@gmail.com>
2016-03-01 20:55:44 +0900

    投稿フォーム作成

--- app/controllers/ongoings_controller.rb
+++ app/controllers/ongoings_controller.rb
@@ -1,7 +1,14 @@
 class OngoingsController < ApplicationController
 
   def index
-    @ongoings = Ongoing.all
+    @ongoings = Ongoing.includes(:user).order("created_at DESC")
+  end
+
+  def new
+  end
+
+  def create
+    Ongoing.create(projectname: ongoing_params[:projectname], projectslide: ongoing_params[:projectslide], insight: ongoing_params[:insight], user_id: current_user.id, image: ongoing_params[:image], summary: ongoing_params[:summary])
   end
 
 end
