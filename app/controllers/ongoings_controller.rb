cbe6f54f7efe1207b46e4aeef529c81294160944
kawasaki yuto <smile.holic.07p15@gmail.com>
2016-03-01 21:29:20 +0900

    ongoing削除機能実装

--- app/controllers/ongoings_controller.rb
+++ app/controllers/ongoings_controller.rb
@@ -7,8 +7,23 @@ class OngoingsController < ApplicationController
   def new
   end
 
+  def destroy
+    ongoing = Ongoing.find(params[:id])
+    if ongoing.user_id == current_user.id
+      ongoing.destroy
+    end
+  end
+
+  def show
+    @ongoing = Ongoing.find(params[:id])
+  end
+
   def create
     Ongoing.create(projectname: ongoing_params[:projectname], projectslide: ongoing_params[:projectslide], insight: ongoing_params[:insight], user_id: current_user.id, image: ongoing_params[:image], summary: ongoing_params[:summary])
   end
+
+
+
+
 
 end
