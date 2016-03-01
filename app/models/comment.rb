aaa829ed314a769abdef7bf87dc67a2e20474d01
kawasaki yuto <smile.holic.07p15@gmail.com>
2016-03-01 20:07:11 +0900

    オンゴーイングのビュー表示まで

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :twig
   belongs_to :user
+  belongs_to :ongoing
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160301051936) do
+ActiveRecord::Schema.define(version: 20160301083552) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
     t.integer  "twig_id",    limit: 4
     t.text     "text",       limit: 65535
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "ongoings", force: :cascade do |t|
+    t.string   "projectname",  limit: 255
+    t.string   "projectslide", limit: 255
+    t.text     "insight",      limit: 65535
+    t.integer  "user_id",      limit: 4
+    t.string   "summary",      limit: 255
+    t.string   "image",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
